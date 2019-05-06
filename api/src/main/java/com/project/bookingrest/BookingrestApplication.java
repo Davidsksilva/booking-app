421f5663a20f4e0f6b2b80fc1c25693720027c19
David <davidsimonkastle@gmail.com>
2019-05-06 17:58:32 -0300

    First commit

---
+++ api/src/main/java/com/project/bookingrest/BookingrestApplication.java
@@ -0,0 +1,13 @@
+package com.project.bookingrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingrestApplication.class, args);
+    }
+
+}
