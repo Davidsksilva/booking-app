48dde641561c763453d780b4687205ba22ebb808
David <davidsimonkastle@gmail.com>
2019-05-08 17:51:08 -0300

    Axios

--- public/booking-app/src/pages/Main/index.js
+++ public/booking-app/src/pages/Main/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import { 
-  Layout, Menu, Breadcrumb, Row, Col, Input, Button, Icon, Form, DatePicker, InputNumber, Select,Alert
+  Layout, Menu, Breadcrumb, Row, Col, Input, Button, Icon, Form, DatePicker, InputNumber, Select,Alert,
+  Table, Divider, Tag
    } from 'antd';
 import "antd/dist/antd.css";
 import "./styles.css"
@@ -10,13 +12,96 @@ const { Header, Content, Footer } = Layout;
 const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
 const Option = Select.Option;
 
+const placesList = ["AC", "AL", "AP","AM","BA","CE",
+"DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RR","RO",
+"RJ","RN","RS","SC","SP","SE","TO"]
+
 // App's Main Page
 export default class Main extends Component {
 
+  state = {
+    hotelDataSource: [],
+    hotelColumns: [{
+      title: 'Nome',
+      dataIndex: 'name',
+      key: 'name',
+    }, {
+      title: 'Estado',
+      dataIndex: 'state',
+      key: 'state',
+    }, {
+      title: 'Estrelas',
+      dataIndex: 'stars',
+      key: 'stars',
+    }],
+    destination: "AP",
+    origin: "AC",
+    error: false,
+  }
+
+  generateDestOptions = ()=>{
+
+
+
+  }
 
   onChange = (e) => {
     console.log(e);
   };
+
+  handleChangeDestination = (value)=>{
+
+    this.setState({
+      destination: value,
+    })
+  }
+
+  handleChangeOrigin = (value)=>{
+
+    this.setState({
+      origin: value
+    })
+
+    console.log(this.state.origin)
+  }
+
+
+  makeRequest = async () =>{
+
+    this.setState({hotelDataSource: []});
+
+    axios.get(`http://localhost:9090/hoteis?location=${this.state.destination}`)
+    .then((response) =>{
+
+  
+      let hotel_list = response.data._embedded.hotelList
+
+      hotel_list.map(hotel =>{
+        let hotel_data = {
+          key: hotel.id,
+          name: "Hotel " + hotel.name,
+          state: hotel.state,
+          stars: hotel.stars
+        }
+
+        //console.log(hotel_data)
+        //console.log(this.state.hotelsDataSource)
+        this.setState({ hotelDataSource: [...this.state.hotelDataSource, hotel_data] })
+
+        console.log(hotel_data);
+      })
+      
+
+    })
+    .catch((error)=>{
+
+    }).finally(() =>{
+
+    }
+      
+    );
+
+  }
 
   handleChange(value) {
     console.log(`selected ${value}`);
@@ -60,42 +145,20 @@ export default class Main extends Component {
                   <Col className = "Col-Container" >
                   <Select 
                   showSearch
-                  size = "medium"
+                  size = "default"
                   allowClear
-                  emptyText = "Oi"
                   placeholder="Local"
                   optionFilterProp="children"
-                  onChange={this.onChange}
+                  onChange={this.handleChangeOrigin}
                   filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
                   defaultValue="AC" 
                   style={{ width: 120 }}>
-                  <Option value ="AC">AC</Option>
-                    <Option value ="AL">AL</Option>
-                    <Option value ="AP">AP</Option>
-                    <Option value ="AM">AM</Option>
-                    <Option value ="BA">BA</Option>
-                    <Option value ="CE">CE</Option>
-                    <Option value ="DF">DF</Option>
-                    <Option value ="ES">ES</Option>
-                    <Option value ="GO">GO</Option>
-                    <Option value ="MA">MA</Option>
-                    <Option value ="MT">MT</Option>
-                    <Option value ="MS">MS</Option>
-                    <Option value ="MG">MG</Option>
-                    <Option value ="PA">PA</Option>
-                    <Option value ="PB">PB</Option>
-                    <Option value ="PR">PR</Option>
-                    <Option value ="PE">PE</Option>
-                    <Option value ="PI">PI</Option>
-                    <Option value ="RR">RR</Option>
-                    <Option value ="RO">RO</Option>
-                    <Option value ="RJ">RJ</Option>
-                    <Option value ="RN">RN</Option>
-                    <Option value ="RS">RS</Option>
-                    <Option value ="SC">SC</Option>
-                    <Option value ="SP">SP</Option>
-                    <Option value ="SE">SE</Option>
-                    <Option value ="TO">TO</Option>
+                    {placesList.map(p => {
+                      if(this.state.destination === p)
+                        return (<Option key={p} disabled>{p}</Option>)
+                      else 
+                      return (<Option key={p}>{p}</Option>)
+                    })}
                   </Select>
                   </Col>
                   <Col className = "Col-Container" >
@@ -104,41 +167,20 @@ export default class Main extends Component {
                   <Col className = "Col-Container" >
                   <Select 
                   showSearch
-                  size = "medium"
+                  size = "default"
                   allowClear
                   placeholder="Local"
                   optionFilterProp="children"
-                  onChange={this.onChange}
+                  onChange={this.handleChangeDestination}
                   filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
                   defaultValue="AL" 
                   style={{ width: 120 }}>
-                  <Option value ="AC">AC</Option>
-                    <Option value ="AL">AL</Option>
-                    <Option value ="AP">AP</Option>
-                    <Option value ="AM">AM</Option>
-                    <Option value ="BA">BA</Option>
-                    <Option value ="CE">CE</Option>
-                    <Option value ="DF">DF</Option>
-                    <Option value ="ES">ES</Option>
-                    <Option value ="GO">GO</Option>
-                    <Option value ="MA">MA</Option>
-                    <Option value ="MT">MT</Option>
-                    <Option value ="MS">MS</Option>
-                    <Option value ="MG">MG</Option>
-                    <Option value ="PA">PA</Option>
-                    <Option value ="PB">PB</Option>
-                    <Option value ="PR">PR</Option>
-                    <Option value ="PE">PE</Option>
-                    <Option value ="PI">PI</Option>
-                    <Option value ="RR">RR</Option>
-                    <Option value ="RO">RO</Option>
-                    <Option value ="RJ">RJ</Option>
-                    <Option value ="RN">RN</Option>
-                    <Option value ="RS">RS</Option>
-                    <Option value ="SC">SC</Option>
-                    <Option value ="SP">SP</Option>
-                    <Option value ="SE">SE</Option>
-                    <Option value ="TO">TO</Option>
+                    {placesList.map(p => {
+                      if(this.state.origin === p)
+                        return (<Option key={p} disabled>{p}</Option>)
+                      else 
+                      return (<Option key={p}>{p}</Option>)
+                    })}
                   </Select>
                   </Col>
                   <Col className = "Col-Container">
@@ -154,15 +196,15 @@ export default class Main extends Component {
                   <InputNumber min={1} max={10} defaultValue={1} onChange={this.onChange} />
                   </Col>
                   <Col className = "Col-Container" id="End" >
-                    <Button type="dashed" icon="search">Buscar</Button>
+                    <Button type="dashed" icon="search" onClick={this.makeRequest}>Buscar</Button>
                   </Col>
                   </Row>
                 </Col>
 
                 </Row> 
 
-              <Row id = "Input-Alert">
-                <Alert
+                {this.state.error?<Row id = "Input-Alert">
+               <Alert
                  
                   message="Não foi possível encontrar o pacote pesquisado"
                   type="error"
@@ -170,6 +212,11 @@ export default class Main extends Component {
                   banner
                   onClose={this.onClose}
                 />
+              </Row> : ''}
+
+              <Row id= "Hotel-Table">
+
+              <Table dataSource={this.state.hotelDataSource} columns={this.state.hotelColumns} />
               </Row>
             </Col>
 
